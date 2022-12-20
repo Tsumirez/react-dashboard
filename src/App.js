45c955618fc92c05102cb597d60759a327e1c133
zefirez <tsumirez@zoho.com>
2022-12-20 13:56:48 +0100

    TopBar component layout complete, light/dark mode button operational.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./scenes/global/TopBar";
 
 function App() {
   const [theme, coloreMode] = useMode();
@@ -7,8 +8,11 @@ function App() {
   return (
     <ColorModeContext.Provider value={coloreMode}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <div className="app">
-
+          <main className="content">
+            <TopBar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
